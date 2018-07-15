8b4a8f6b4dbb5648d0341b46b95e0516076e9e2a
Nathan <nlincoln@intellifarms.com>
2018-07-15 00:14:18 -0500

    baseame -> basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Watcher from "./routes/Watcher";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter baseame={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <React.Fragment>
           <Route exact path={"/"} component={Analyzer} />
           <Route path={"/watcher"} component={Watcher} />
