05fd49eaa12180c1af545b50a8824d06bab442ea
Nathan <nlincoln@intellifarms.com>
2018-07-15 00:17:25 -0500

    Use the hash router, it plays nicer with github pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, HashRouter } from "react-router-dom";
 import Analyzer from "./routes/Analyzer";
 import Watcher from "./routes/Watcher";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <React.Fragment>
           <Route exact path={"/"} component={Analyzer} />
           <Route path={"/watcher"} component={Watcher} />
         </React.Fragment>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
